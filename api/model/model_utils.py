95e016a261fb9fc6ef6c30b4a231281ba25570b6
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2023-11-21 10:55:31 +0100

    api working

---
+++ api/model/model_utils.py
@@ -0,0 +1,53 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import custom_object_scope
+from transformers import TFCamembertModel, CamembertTokenizer
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+
+def load() :
+## Loading model and tokenizer
+    with custom_object_scope({'TFCamembertModel': TFCamembertModel}):
+        model = load_model('saved_model/test.h5')
+        tokenizer = CamembertTokenizer.from_pretrained("jplu/tf-camembert-base")
+    return model, tokenizer
+
+def label_extractor(path="labels.csv", col='labels'):
+    df = pd.read_csv(path)
+    return df[col]
+
+def label_encoder():
+    # Création de l'encodeur
+    le = LabelEncoder()
+    y = label_extractor()
+    # Entraînement de l'encodeur et transformation des labels
+    le.fit_transform(y)
+    return le
+
+
+def encode_texts(texts, tokenizer, max_seq_length):
+    input_ids = []
+    for text in texts:
+        encoded = tokenizer(text, return_tensors="tf", truncation=True, padding="max_length", max_length=max_seq_length)
+        input_ids.append(encoded["input_ids"][0])
+    return np.array(input_ids)
+
+def prediction(model, max_seq_length, tokenizerf, *args) :
+    texts = list(*args)
+    to_test = encode_texts(texts=texts, tokenizer=tokenizerf, max_seq_length=max_seq_length)
+    proba = model.predict(to_test)
+    indexes = np.argmax(proba, axis=1)
+    le = label_encoder()
+    return le.inverse_transform(indexes)
+
+def predict_pipeline(to_predict:list, max_seq_length=32):
+    model, tokenizer = load()
+    labels = label_extractor(path="labels.csv")
+    le = label_encoder()
+    preds = prediction(model, max_seq_length, tokenizer, to_predict)
+
+    return preds
+
+
+
+
---
+++ api/model/test.py
@@ -0,0 +1,16 @@
+import model_utils
+
+# model, tokenizer = model_utils.load()
+# labels = model_utils.label_extractor(path="labels.csv")
+# le = model_utils.label_encoder()
+
+test1 = 'defaut de com ipc'
+test2 = 'blabla hmi cassé'
+# max_seq_length = 32
+
+# preds = model_utils.prediction(model, max_seq_length, tokenizer, [test1, test2])
+
+# print(preds)
+
+
+print(model_utils.predict_pipeline([test1,test2]))

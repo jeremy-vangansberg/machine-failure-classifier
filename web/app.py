4f58bb3a83046bc2f299d086572a0f4720424bc0
Emad DARWICH <108935365+emaddar@users.noreply.github.com>
2023-11-23 16:11:22 +0100

    app updated to add "le pourcentage de lignes prédit comme 'à catégoriser par un humain'

--- web/app.py
+++ web/app.py
@@ -74,7 +74,6 @@ def main():
                 
                 
             
-                        st.write(f"Temps de calcul : {round(end_time - start_time, 2)} secondes")
                         
 
 
@@ -88,6 +87,10 @@ def main():
                                     st.write("")
                                     st.write("")
                                     st.write("")
+                                    # Calculate the percentage
+                                    percentage = (len(df[df['sous ensemble estimé'] == 'à catégoriser par un humain']) / len(df)) * 100
+                                    st.write(f"le pourcentage de lignes prédit comme 'à catégoriser par un humain' : {percentage}")
+                                    st.write(f"Temps de calcul : {round(end_time - start_time, 2)} secondes")
 
                                     df_xlsx = to_excel(df_pred)   
                                     st.download_button(label='📥 Download Current Result',
